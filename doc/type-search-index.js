9f65fd63d0e60d21a55d16e5ffe9437c7e60a72c
Vinicius Kalach <viniciuscato@hotmail.com>
2023-11-08 00:29:25 -0300

    criar javadoc

---
+++ doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"teste","l":"CalculadoraJUnitTest"},{"p":"calculadora","l":"Divisao"},{"p":"calculadora","l":"Multiplicacao"},{"p":"calculadora","l":"Soma"},{"p":"calculadora","l":"Subtracao"}];updateSearchResults();
